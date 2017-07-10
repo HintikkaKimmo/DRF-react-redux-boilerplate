c95b25262e3122608ea7d1b896e179212c3be92f
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-07-10 11:33:32 +0100

    corrected the dirjoin syntax errror in MEDIA_ROOT within settings/base.py

--- application_configuration/settings/base.py
+++ application_configuration/settings/base.py
@@ -208,7 +208,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(os.path.dirname((BASE_DIR), "media_cdn"))
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 
 # DJANGO REST FRAMEWORK
